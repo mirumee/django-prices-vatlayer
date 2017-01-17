198beb9fa071ba643a7f1811f38b29a62c43a031
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-17 12:57:35 +0100

    Initial package version

---
+++ setup.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+import os
+from setuptools import setup
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_settings')
+
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules']
+
+setup(
+    name='django-prices-vatlayer',
+    author='Mirumee Software',
+    author_email='hello@mirumee.com',
+    description='vatlayer.org support for django',
+    license='BSD',
+    url='https://github.com/mirumee/django-prices-vatlayer',
+    packages=[
+        'django_prices_vatlayer'],
+    include_package_data=True,
+    classifiers=CLASSIFIERS,
+    install_requires=['Django>=1.4'],
+    platforms=['any'],
+    test_suite='django_prices_vatlayer.tests',
+    zip_safe=False)
+
