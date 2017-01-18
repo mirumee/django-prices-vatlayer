b158e2d5b9098ebdc456522d583a895197452865
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-18 15:58:26 +0100

    Add function to validate data and get european vat types

--- django_prices_vatlayer/utils.py
+++ django_prices_vatlayer/utils.py
@@ -13,6 +13,13 @@ except AttributeError:
     raise ImproperlyConfigured('VATLAYER_ACCESS_KEY is required')
 
 RATINGS_URL = 'http://apilayer.net/api/rate_list'
+TYPES_URL = 'http://apilayer.net/api/types'
+
+
+def validate_data(json_data):
+    if not json_data['success']:
+        info = json_data['error']['info']
+        raise ImproperlyConfigured(info)
 
 
 def get_european_vat_rates():
@@ -20,11 +27,15 @@ def get_european_vat_rates():
     return response.json()
 
 
+def get_european_vat_types():
+    response = requests.get(TYPES_URL, params={'access_key': ACCESS_KEY})
+    json_data = response.json()
+    validate_data(json_data)
+    return json_data['types']
+
+
 def create_objects_from_json(json_data):
-    if not json_data['success']:
-        # handle errors
-        info = json_data['error']['info']
-        raise ImproperlyConfigured(info)
+    validate_data(json_data)
 
     # Handle proper response
     rates = json_data['rates']
