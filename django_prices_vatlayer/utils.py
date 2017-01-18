baed8a2a382b99aaa4b19ccdaaa61a0e3616b08b
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-18 13:50:49 +0100

    Handle proper json response and save to database

--- django_prices_vatlayer/utils.py
+++ django_prices_vatlayer/utils.py
@@ -2,6 +2,7 @@ import requests
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.utils import six
 
 from .models import Vat
 
@@ -17,3 +18,15 @@ RATINGS_URL = 'http://apilayer.net/api/rate_list'
 def get_european_vat_rates():
     response = requests.get(RATINGS_URL, params={'access_key': ACCESS_KEY})
     return response.json()
+
+
+def create_objects_from_json(json_data):
+    if not json_data['success']:
+        # handle errors
+        pass
+
+    # Handle proper response
+    rates = json_data['rates']
+    for code, value in six.iteritems(rates):
+        Vat.objects.update_or_create(
+             country_code=code, defaults={'data': value})
