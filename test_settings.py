9e8fb9e38b8e4b927acdca5cb8c1d258a7b32c9e
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-18 14:55:01 +0100

    Define test database in settings

--- test_settings.py
+++ test_settings.py
@@ -2,8 +2,12 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': ':memory:'}}
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vat',
+        'user': '',
+        'password': '',
+    }
+}
 
 SECRET_KEY = 'irrelevant'
 INSTALLED_APPS = [
