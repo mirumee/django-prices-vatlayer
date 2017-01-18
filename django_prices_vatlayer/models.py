129c2fb6179a9a203da3318a2627f400ec50c743
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-18 13:18:47 +0100

    Model is python2 compatible

--- django_prices_vatlayer/models.py
+++ django_prices_vatlayer/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.utils.translation import pgettext_lazy
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class Vat(models.Model):
     country_code = models.CharField(
         pgettext_lazy('Vat field', 'country code'), max_length=2)
     data = JSONField(pgettext_lazy('Vat field', 'data'))
+
+    def __str__(self):
+        return self.country_code
