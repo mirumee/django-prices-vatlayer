bcb120da8546ab457b6d8afc78e3b2467cbf9af0
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-19 11:44:47 +0100

    Test create objects from json

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,5 +17,7 @@ def json_error():
 
 @pytest.fixture
 def json_success():
-    data = {'success': True}
+    data = {'success': True, 'rates': {
+        "AT": {"country_name": "Austria", "standard_rate": 20,
+               "reduced_rates": {"foodstuffs": 10, "books": 10}}}}
     return data
--- tests/test_vatlayer.py
+++ tests/test_vatlayer.py
@@ -13,3 +13,18 @@ def test_validate_data(json_error, json_success):
         validate_data(json_error)
 
     assert validate_data(json_success) is None
+
+
+@pytest.mark.django_db
+def test_create_objects_from_json(json_error, json_success):
+    from django_prices_vatlayer.utils import create_objects_from_json
+    from django_prices_vatlayer.models import Vat
+
+    vat_counts = Vat.objects.count()
+
+    with pytest.raises(ImproperlyConfigured):
+        create_objects_from_json(json_error)
+    assert vat_counts == Vat.objects.count()
+
+    create_objects_from_json(json_success)
+    assert vat_counts + 1 == Vat.objects.count()
