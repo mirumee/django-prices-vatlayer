b06ef21fa81c105c2a1eef898748ffa58f2de457
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-19 15:18:14 +0100

    Refactor imports in test module

--- tests/test_vatlayer.py
+++ tests/test_vatlayer.py
@@ -4,49 +4,46 @@ import django
 from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
 from django.core.management import call_command
 
+from django_prices_vatlayer import utils
 
 django.setup()
 
 
 def test_validate_data(json_error, json_success):
-    from django_prices_vatlayer.utils import validate_data
     with pytest.raises(ImproperlyConfigured):
-        validate_data(json_error)
+        utils.validate_data(json_error)
 
-    assert validate_data(json_success) is None
+    assert utils.validate_data(json_success) is None
 
 
 @pytest.mark.django_db
 def test_create_objects_from_json(json_error, json_success):
-    from django_prices_vatlayer.utils import create_objects_from_json
     from django_prices_vatlayer.models import Vat
 
     vat_counts = Vat.objects.count()
 
     with pytest.raises(ImproperlyConfigured):
-        create_objects_from_json(json_error)
+        utils.create_objects_from_json(json_error)
     assert vat_counts == Vat.objects.count()
 
-    create_objects_from_json(json_success)
+    utils.create_objects_from_json(json_success)
     assert vat_counts + 1 == Vat.objects.count()
 
 
 def test_get_tax_for_country(vat_country):
-    from django_prices_vatlayer.utils import get_tax_for_country
-
     with pytest.raises(ObjectDoesNotExist):
-        get_tax_for_country('XX', 'rate name')
+        utils.get_tax_for_country('XX', 'rate name')
 
-    standard_rate = get_tax_for_country(vat_country.country_code, 'medicine')
+    country_code = vat_country.country_code
+    standard_rate = utils.get_tax_for_country(country_code, 'medicine')
     assert standard_rate == 20
 
-    reduced_rate = get_tax_for_country(vat_country.country_code, 'books')
+    reduced_rate = utils.get_tax_for_country(country_code, 'books')
     assert reduced_rate == 10
 
 
 @pytest.mark.django_db
 def test_get_vat_rates_command(monkeypatch, json_success):
-    from django_prices_vatlayer import utils
     from django_prices_vatlayer.models import Vat
     monkeypatch.setattr(utils, 'get_european_vat_rates',
                         lambda: json_success)
