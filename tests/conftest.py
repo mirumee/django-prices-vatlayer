47d97922f8365a2d40c77d516c5390a6f8a98647
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-19 11:27:16 +0100

    Test validate_data function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,21 @@
+import pytest
 import os
 
 
 def pytest_configure():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
+
+
+@pytest.fixture
+def json_error():
+    data = {
+        'success': False,
+        'error': {'info': 'Invalid json'}
+    }
+    return data
+
+
+@pytest.fixture
+def json_success():
+    data = {'success': True}
+    return data
--- tests/test_vatlayer.py
+++ tests/test_vatlayer.py
@@ -1,2 +1,15 @@
 import pytest
 
+import django
+from django.core.exceptions import ImproperlyConfigured
+
+
+django.setup()
+
+
+def test_validate_data(json_error, json_success):
+    from django_prices_vatlayer.utils import validate_data
+    with pytest.raises(ImproperlyConfigured):
+        validate_data(json_error)
+
+    assert validate_data(json_success) is None
