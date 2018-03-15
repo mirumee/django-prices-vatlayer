0310f903ecf68a3e68ad97b68a362cda751ecced
Rafał Pitoń <rafal@mirumee.com>
2018-03-15 15:59:48 +0100

    Remove py2 compat layer

--- django_prices_vatlayer/models.py
+++ django_prices_vatlayer/models.py
@@ -1,13 +1,11 @@
 from jsonfield import JSONField
 
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import pgettext_lazy
 
 DEFAULT_TYPES_INSTANCE_ID = 1
 
 
-@python_2_unicode_compatible
 class VAT(models.Model):
     country_code = models.CharField(
         pgettext_lazy('Vat field', 'country code'), max_length=2)
