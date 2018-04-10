93b180ffc63ca845fd5a491e52eff7ed683a66f3
Anna Siwik <anna.siwik@mirumee.com>
2018-04-10 12:38:09 +0200

    Bump version to 1.0.1 in setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author_email='hello@mirumee.com',
     description='vatlayer.com support for django',
     license='BSD',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/mirumee/django-prices-vatlayer',
     packages=[
         'django_prices_vatlayer', 'django_prices_vatlayer.migrations',
